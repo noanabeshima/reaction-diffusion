60002140e2f180261244b67466a54c83c9c14e4c
Noa Nabeshima <noanabeshima@gmail.com>
2020-02-07 01:16:11 -0800

    made changes to fit PEP8 and made code more readable

--- diff.py
+++ diff.py
@@ -1,46 +1,53 @@
 import numpy as np
-import time
 import cv2
 
+
 def roll(A, *coord):
+    # Returns a shifted version of the array
+    # See np.roll for more information
     if (len(A.shape) != len(coord)):
-        raise Exception('Number of dimensions of input array ['+str(len(A.shape))+\
-                        '] and coordinate for rolling ['+str(len(coord))+'] don\'t match')
+        raise Exception('Number of dimensions of input array [' + str(len(A.shape)) +
+                        '] and coordinate for rolling [' + str(len(coord)) + '] don\'t match')
     for i in range(len(coord)):
         if coord[i] != 0:
             A = np.roll(A, coord[i], axis=i)
     return A
 
-def lap(A):
-    result = -A +.2*(np.roll(A, 1, axis = 0)+np.roll(A, 1, axis=1)+np.roll(A, -1, axis=0)+np.roll(A,-1, axis=1))+.05*(roll(A, 1, 1)+roll(A, 1, -1)+roll(A, -1, 1)+roll(A, -1, -1))
+
+def laplacian(A):
+    result = -A + .2 * (np.roll(A, 1, axis=0) + np.roll(A, 1, axis=1) + np.roll(A, -1, axis=0) +
+                        np.roll(A, -1, axis=1)) + .05 * (roll(A, 1, 1) + roll(A, 1, -1) +
+                                                         roll(A, -1, 1) + roll(A, -1, -1))
     return result
 
-PPS = 4
+
+pixels_per_square = 2
 X, Y = 650, 400
 
-A = np.ones((Y,X))
-B = np.zeros((Y,X))
-B+=np.random.randn(Y,X)/5.55
+A = np.ones((Y, X))
+B = np.zeros((Y, X))
+B += np.random.randn(Y, X) / 5.55
 
+# "physical" constants described in Sim's page
 dt = 1
 k = .048
 f = .012
 DA = 1
 DB = .5
 
-im = np.kron(1-A, np.ones((PPS, PPS)))
-cv2.imshow('Gray-Scott',im)
+im = np.kron(1 - A, np.ones((pixels_per_square, pixels_per_square)))
+cv2.imshow('Gray-Scott', im)
 cv2.waitKey(1)
 cv2.setWindowProperty('Gray-Scott', 0, 1.)
 cv2.setWindowProperty('Gray-Scott', 1, 1.)
 
 
-for i in range(100000):    
-    im = np.kron(A, np.ones((PPS, PPS)))
-    cv2.imshow('Gray-Scott',im)
-    if cv2.waitKey(1)==27:
+while True:
+    im = np.kron(A, np.ones((pixels_per_square, pixels_per_square)))
+    cv2.imshow('Gray-Scott', im)
+    if cv2.waitKey(1) == 27:
         break
     cv2.setWindowProperty('Gray-Scott', 0, 1.)
-    
-    A = A + (DA*lap(A)-A*B*B+f*(1-A))*dt
-    B = B + (DB*lap(B)+A*B*B-(k+f)*B)*dt
+
+    A = A + (DA * laplacian(A) - A * B * B + f * (1 - A)) * dt
+    B = B + (DB * laplacian(B) + A * B * B - (k + f) * B) * dt
--- mitosis.py
+++ mitosis.py
@@ -1,41 +1,49 @@
 import numpy as np
-from time import sleep
 import cv2
 
+
 def roll(A, *coord):
+    # Returns a shifted version of the array
+    # See np.roll for more information
     if (len(A.shape) != len(coord)):
-        raise Exception('Number of dimensions of input array ['+str(len(A.shape))+\
-                        '] and coordinate for rolling ['+str(len(coord))+'] don\'t match')
+        raise Exception('Number of dimensions of input array [' + str(len(A.shape)) +
+                        '] and coordinate for rolling [' + str(len(coord)) + '] don\'t match')
     for i in range(len(coord)):
         if coord[i] != 0:
             A = np.roll(A, coord[i], axis=i)
     return A
 
-def lap(A):
-    result = -A +.2*(np.roll(A, 1, axis = 0)+np.roll(A, 1, axis=1)+np.roll(A, -1, axis=0)+np.roll(A,-1, axis=1))+.05*(roll(A, 1, 1)+roll(A, 1, -1)+roll(A, -1, 1)+roll(A, -1, -1))
+
+def laplacian(A):
+    result = -A + .2 * (np.roll(A, 1, axis=0) + np.roll(A, 1, axis=1) + np.roll(A, -1, axis=0) +
+                        np.roll(A, -1, axis=1)) + .05 * (roll(A, 1, 1) + roll(A, 1, -1) +
+                                                         roll(A, -1, 1) + roll(A, -1, -1))
     return result
 
-PPS = 3
-size = 400
 
-A = np.ones((size,size))
-B = np.zeros((size,size))
+pixels_per_square = 3
+X, Y = 650, 400
 
-B+=np.random.randn(size,size)/5.5
+A = np.ones((Y, X))
+B = np.zeros((Y, X))
 
+B += np.random.randn(Y, X) / 5.5
+
+# "physical" constants described in Sim's page
 dt = 1
 k = .055
 f = .015
 DA = 1
 DB = .5
 
+while True:
+    im = np.kron(A, np.ones((pixels_per_square, pixels_per_square)))
+    cv2.imshow('Test', im)
 
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows()
+        break
 
-for i in range(10000):
-    if i%2 == 0:
-        im = np.kron(A, np.ones((PPS, PPS)))
-        cv2.imshow('Test',im)
-        cv2.waitKey(1)
-
-    A = A + (DA*lap(A)-A*B*B+f*(1-A))*dt
-    B = B + (DB*lap(B)+A*B*B-(k+f)*B)*dt
+    # update arrays
+    A = A + (DA * laplacian(A) - A * B * B + f * (1 - A)) * dt
+    B = B + (DB * laplacian(B) + A * B * B - (k + f) * B) * dt
